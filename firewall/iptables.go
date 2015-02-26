5a85fde0b83130c4612eb1b509dd524e0068df13
Javier Perez-Griffo <javier@besol.es>
2015-02-26 13:04:05 +0100

    Inclusion of newline after build spec

--- firewall/iptables.go
+++ firewall/iptables.go
@@ -16,7 +16,7 @@ func apply(policy Policy) error {
 	utils.RunCmd("iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
 
 	for _, rule := range policy.Rules {
-		utils.RunCmd(fmt.Sprintf("iptables -A INPUT -s %s -p %s --dport %d:%d -j ACCEPT\n", rule.Cidr, rule.Protocol, rule.MinPort, rule.MaxPort))
+		utils.RunCmd(fmt.Sprintf("iptables -A INPUT -s %s -p %s --dport %d:%d -j ACCEPT", rule.Cidr, rule.Protocol, rule.MinPort, rule.MaxPort))
 	}
 	utils.RunCmd("iptables -P INPUT ACCEPT")
 	utils.RunCmd("iptables -F INPUT")
