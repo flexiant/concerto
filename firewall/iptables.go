e6a6587a5a37be53fac896f57f7b78594232a3f7
Javier Perez-Griffo <javier@besol.es>
2015-05-01 17:26:36 +0200

    Inclusion of Iptables Path

--- firewall/iptables.go
+++ firewall/iptables.go
@@ -12,20 +12,20 @@ func driverName() string {
 }
 
 func apply(policy Policy) error {
-	utils.RunCmd("iptables -F INPUT")
-	utils.RunCmd("iptables -P INPUT DROP")
-	utils.RunCmd("iptables -A INPUT -i lo -j ACCEPT")
-	utils.RunCmd("iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
+	utils.RunCmd("/sbin/iptables -F INPUT")
+	utils.RunCmd("/sbin/iptables -P INPUT DROP")
+	utils.RunCmd("/sbin/iptables -A INPUT -i lo -j ACCEPT")
+	utils.RunCmd("/sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
 
 	for _, rule := range policy.Rules {
-		utils.RunCmd(fmt.Sprintf("iptables -A INPUT -s %s -p %s --dport %d:%d -j ACCEPT", rule.Cidr, rule.Protocol, rule.MinPort, rule.MaxPort))
+		utils.RunCmd(fmt.Sprintf("/sbin/iptables -A INPUT -s %s -p %s --dport %d:%d -j ACCEPT", rule.Cidr, rule.Protocol, rule.MinPort, rule.MaxPort))
 	}
 
 	return nil
 }
 
 func flush() error {
-	utils.RunCmd("iptables -P INPUT DROP")
-	utils.RunCmd("iptables -F INPUT")
+	utils.RunCmd("/sbin/iptables -P INPUT DROP")
+	utils.RunCmd("/sbin/iptables -F INPUT")
 	return nil
 }
