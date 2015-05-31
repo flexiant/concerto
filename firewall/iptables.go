06c6e4ed053cd33b3d9a9316173a8f8206f41c63
Javier Perez-Griffo <javier@besol.es>
2015-05-31 22:52:56 +0200

    Minor modification to Iptables

--- firewall/iptables.go
+++ firewall/iptables.go
@@ -15,14 +15,25 @@ func apply(policy Policy) error {
 	utils.RunCmd("/sbin/iptables -w -N CONCERTO")
 	utils.RunCmd("/sbin/iptables -w -F CONCERTO")
 	utils.RunCmd("/sbin/iptables -w -P INPUT DROP")
-	utils.RunCmd("/sbin/iptables -w -A INPUT -i lo -j ACCEPT")
-	utils.RunCmd("/sbin/iptables -w -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
+
+	_, exitCode, _, _ := utils.RunCmd("/sbin/iptables -w -C INPUT -i lo -j ACCEPT")
+	if exitCode != 0 {
+		utils.RunCmd("/sbin/iptables -w -A INPUT -i lo -j ACCEPT")
+	}
+
+	_, exitCode, _, _ := utils.RunCmd("/sbin/iptables -w -C INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
+	if exitCode != 0 {
+		utils.RunCmd("/sbin/iptables -w -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
+	}
 
 	for _, rule := range policy.Rules {
 		utils.RunCmd(fmt.Sprintf("/sbin/iptables -w -A CONCERTO -s %s -p %s --dport %d:%d -j ACCEPT", rule.Cidr, rule.Protocol, rule.MinPort, rule.MaxPort))
 	}
 
-	utils.RunCmd("/sbin/iptables -w -C INPUT -j CONCERTO && /sbin/iptables -w -A INPUT -j CONCERTO")
+	_, exitCode, _, _ := utils.RunCmd("/sbin/iptables -w -C INPUT -j CONCERTO")
+	if exitCode != 0 {
+		utils.RunCmd("/sbin/iptables -w -A INPUT -j CONCERTO")
+	}
 
 	return nil
 }
