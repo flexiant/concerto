e37ea7167ef45e19bd1c80ce9dbc98e89094df93
Pablo Mercado <odacremolbap@gmail.com>
2015-10-14 11:00:13 +0200

    fix min_port and max_port order in struct

--- firewall/firewall.go
+++ firewall/firewall.go
@@ -26,8 +26,8 @@ type Policy struct {
 type Rule struct {
 	Protocol string `json:"ip_protocol"`
 	Cidr     string `json:"cidr_ip"`
-	MaxPort  int    `json:"max_port"`
 	MinPort  int    `json:"min_port"`
+	MaxPort  int    `json:"max_port"`
 }
 
 func list(policy Policy) error {
