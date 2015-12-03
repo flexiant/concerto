afc7732d65cf13d8d06c7dfd187fcb6bb10e321a
Javi Perez-Griffo <javi@flexiant.com>
2015-12-03 11:45:27 +0100

    Inclusion of script to install githooks

--- githooks/install.bash
+++ githooks/install.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 pwd=`git rev-parse --show-toplevel`
-echo cp $pwd/githooks/pre-commit $pwd/.git/hooks/
+cp $pwd/githooks/pre-commit $pwd/.git/hooks/
--- utils/version.go
+++ utils/version.go
@@ -1,3 +1,3 @@
 package utils
 
-const VERSION = "0.3.11"
+const VERSION = "0.3.12"
