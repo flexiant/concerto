c127f30b4911cc2e42e3727ee88d621c9429daed
Diana Crisan <dcrisan@flexiant.com>
2016-02-03 22:26:15 +0000

    Adding tests for domain update and domain delete

--- cmd/domain_test.go
+++ cmd/domain_test.go
@@ -161,6 +161,95 @@ func TestDomainCreate(t *testing.T) {
 
 }
 
+// TestDomainUpdate subcommand
+func TestDomainUpdate(t *testing.T) {
+
+	assert := assert.New(t)
+
+	domainsTest, err := GetDomainData()
+	assert.Nil(err, "Couldn't load domain test data")
+
+	// only valid domains
+	var domainsIn []api.Domain
+	for _, domainTest := range domainsTest {
+		if domainTest.Valid {
+			domainsIn = append(domainsIn, domainTest.Domain)
+		}
+	}
+
+	// wire up
+	cs := &utils.MockConcertoService{}
+	ds, err := api.NewDomainService(cs)
+	assert.Nil(err, "Couldn't load domain service")
+	assert.NotNil(ds, "Domain service not instanced")
+
+	for _, domainIn := range domainsIn {
+
+		// convertMap
+		mapIn, err := itemConvertParams(domainIn)
+		assert.Nil(err, "Domain test data corrupted")
+
+		// to json
+		dOut, err := json.Marshal(domainIn)
+		assert.Nil(err, "Domain test data corrupted")
+
+		// call service
+		cs.On("Put", fmt.Sprintf("/v1/dns/domains/%s", domainIn.ID), mapIn).Return(dOut, 200, nil)
+		domainOut, err := ds.UpdateDomain(mapIn, domainIn.ID)
+		assert.Nil(err, "Error creating domain list")
+		assert.Equal(domainIn, *domainOut, "GetDomainList returned different domains")
+
+		// TODO iterate all formatters
+		// write output
+		var b bytes.Buffer
+		mockOut := bufio.NewWriter(&b)
+		f := format.NewJSONFormatter(mockOut)
+		assert.NotNil(f, "Formatter")
+		err = f.PrintList(domainOut)
+		assert.Nil(err, "JSON Formatter Printlinst error")
+		mockOut.Flush()
+
+		// TODO add more accurate parsing
+		assert.Regexp("\\{\\\"id\\\":.*\\}", b.String(), "JSON Output didn't match regular expression")
+
+	}
+
+}
+
+// TestDeleteDomain subcommand
+func TestDeleteDomain(t *testing.T) {
+
+	assert := assert.New(t)
+
+	domainsTest, err := GetDomainData()
+	assert.Nil(err, "Couldn't load domain test data")
+
+	// only valid domains
+	var domainsIn []api.Domain
+	for _, domainTest := range domainsTest {
+		if domainTest.Valid {
+			domainsIn = append(domainsIn, domainTest.Domain)
+		}
+	}
+
+	// wire up
+	cs := &utils.MockConcertoService{}
+	ds, err := api.NewDomainService(cs)
+	assert.Nil(err, "Couldn't load domain service")
+	assert.NotNil(ds, "Domain service not instanced")
+
+	for _, domainIn := range domainsIn {
+		// to json
+		dIn, err := json.Marshal(domainIn)
+		assert.Nil(err, "Domain test data corrupted")
+
+		// call service
+		cs.On("Delete", fmt.Sprintf("/v1/dns/domains/%s", domainIn.ID)).Return(dIn, 200, nil)
+		err = ds.DeleteDomain(domainIn.ID)
+		assert.Nil(err, "Error getting domain list")
+	}
+}
+
 type DomainTest struct {
 	Domain api.Domain
 	Valid  bool
@@ -184,7 +273,8 @@ func GetDomainData() ([]DomainTest, error) {
 			Valid: true,
 		},
 		{
-			Domain: api.Domain{ID: "fakeID1",
+			Domain: api.Domain{
+				ID:      "fakeID1",
 				Name:    "fakeName1",
 				TTL:     1001,
 				Contact: "fakeContact1",
