d649052d214285772a630afbf336164f7328cdaa
Pablo Mercado <odacremolbap@gmail.com>
2016-02-11 16:41:24 +0100

    json formatter test

---
+++ utils/format/jsonformatter_test.go
@@ -0,0 +1,29 @@
+package format
+
+import (
+	"bufio"
+	"bytes"
+	"github.com/flexiant/concerto/api"
+	"github.com/flexiant/concerto/testdata"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPrintItemDomain(t *testing.T) {
+
+	assert := assert.New(t)
+	domainsIn := testdata.GetDomainData()
+	domainOut := api.GetDomainListMocked(t, domainsIn)
+
+	var b bytes.Buffer
+	mockOut := bufio.NewWriter(&b)
+	f := NewJSONFormatter(mockOut)
+	assert.NotNil(f, "Formatter")
+
+	err := f.PrintItem(domainOut)
+	assert.Nil(err, "JSON formatter PrintItem error")
+	mockOut.Flush()
+
+	// TODO add more accurate parsing
+	assert.Regexp("\\{\\\"id\\\":.*\\}", b.String(), "JSON Output didn't match regular expression")
+}
