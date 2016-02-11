23bedf88eb5a6db01eeb33bd12ed0bfa6ed5a256
Diana Crisan <dcrisan@flexiant.com>
2016-02-11 23:39:47 +0000

    removing unused data and adding the new subcommands structure

--- blueprint/services/services.go
+++ blueprint/services/services.go
@@ -88,24 +88,3 @@ func cmdShow(c *cli.Context) {
 
 	w.Flush()
 }
-
-func SubCommands() []cli.Command {
-	return []cli.Command{
-		{
-			Name:   "list",
-			Usage:  "Lists all available services",
-			Action: cmdList,
-		},
-		{
-			Name:   "show",
-			Usage:  "Shows information about a specific service",
-			Action: cmdShow,
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "id",
-					Usage: "Service Id",
-				},
-			},
-		},
-	}
-}
--- utils/version.go
+++ utils/version.go
@@ -1,3 +1,3 @@
 package utils
 
-const VERSION = "0.3.32"
+const VERSION = "0.3.33"
