d32bc4733265b5b988af8b083d8430ed04f7924c
Pablo Mercado <odacremolbap@gmail.com>
2016-02-22 16:14:42 +0100

    test txt list formatting

--- utils/format/textformatter_test.go
+++ utils/format/textformatter_test.go
@@ -3,6 +3,7 @@ package format
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"github.com/flexiant/concerto/api"
 	"github.com/flexiant/concerto/testdata"
 	"github.com/stretchr/testify/assert"
@@ -33,25 +34,26 @@ func TestPrintItemDomainTXT(t *testing.T) {
 	}
 }
 
-// func TestPrintListDomains(t *testing.T) {
-//
-// 	assert := assert.New(t)
-// 	domainsIn := testdata.GetDomainData()
-// 	domainOut := api.GetDomainListMocked(t, domainsIn)
-//
-// 	var b bytes.Buffer
-// 	mockOut := bufio.NewWriter(&b)
-// 	InitializeFormatter("json", mockOut)
-// 	f := GetFormatter()
-// 	assert.NotNil(f, "Formatter")
-//
-// 	err := f.PrintList(domainOut)
-// 	assert.Nil(err, "JSON formatter PrintItem error")
-// 	mockOut.Flush()
-//
-// 	// TODO add more accurate parsing
-// 	assert.Regexp("^\\[\\{\\\"id\\\":.*\\}\\]", b.String(), "JSON Output didn't match regular expression")
-// }
+func TestPrintListDomainsTXT(t *testing.T) {
+
+	assert := assert.New(t)
+	domainsIn := testdata.GetDomainData()
+	domainOut := api.GetDomainListMocked(t, domainsIn)
+
+	var b bytes.Buffer
+	mockOut := bufio.NewWriter(&b)
+	InitializeFormatter("text", mockOut)
+	f := GetFormatter()
+	assert.NotNil(f, "Formatter")
+
+	err := f.PrintList(*domainOut)
+	assert.Nil(err, "Text formatter PrintItem error")
+	mockOut.Flush()
+
+	// TODO add more accurate parsing
+	assert.Regexp(fmt.Sprintf("^ID.*\n%s.*\n.*", (*domainOut)[0].ID), b.String(), "JSON Output didn't match regular expression")
+}
+
 //
 // func TestPrintError(t *testing.T) {
 //
