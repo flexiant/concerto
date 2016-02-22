92642e02df65c3118b2d8bce286b83d97f7cec13
Pablo Mercado <odacremolbap@gmail.com>
2016-02-22 12:55:18 +0100

    avoid showing script code in blueprint script lists

--- api/types/scripts.go
+++ api/types/scripts.go
@@ -4,6 +4,6 @@ type Script struct {
 	Id          string   `json:"id" header:"ID"`
 	Name        string   `json:"name" header:"NAME"`
 	Description string   `json:"description" header:"DESCRIPTION"`
-	Code        string   `json:"code" header:"CODE"`
+	Code        string   `json:"code" header:"CODE" show:"nolist"`
 	Parameters  []string `json:"parameters" header:"PARAMETERS"`
 }
