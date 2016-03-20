7e08f892a1b52fa355c209416ccf98ed4b4488fd
Diana Crisan <dcrisan@flexiant.com>
2016-03-20 14:11:51 +0000

    adding the missing code to admin reports tests

--- api/admin_reports_api_mocked.go
+++ api/admin_reports_api_mocked.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/flexiant/concerto/api/types"
 	"github.com/flexiant/concerto/utils"
 	"github.com/stretchr/testify/assert"
@@ -21,7 +22,7 @@ func GetAdminReportListMocked(t *testing.T, adminReportsIn *[]types.Report) *[]t
 
 	// to json
 	arIn, err := json.Marshal(adminReportsIn)
-	assert.Nil(err, "Cluster test data corrupted")
+	assert.Nil(err, "Admin reports test data corrupted")
 
 	// call service
 	cs.On("Get", "/v1/admin/reports").Return(arIn, 200, nil)
@@ -36,7 +37,36 @@ func GetAdminReportListMocked(t *testing.T, adminReportsIn *[]types.Report) *[]t
 		(*adminReportsIn)[i].EndTime = arOut[i].EndTime
 	}
 
-	assert.Equal(*adminReportsIn, arOut, "GetClusterList returned different clusters")
+	assert.Equal(*adminReportsIn, arOut, "GetAdminReportList returned different admin reports")
 
 	return &arOut
+}
+
+// GetAdminReportMocked test mocked function
+func GetAdminReportMocked(t *testing.T, adminReport *types.Report) *types.Report {
+
+	assert := assert.New(t)
+
+	// wire up
+	cs := &utils.MockConcertoService{}
+	ars, err := NewReportService(cs)
+	assert.Nil(err, "Couldn't load adminReport service")
+	assert.NotNil(ars, "AdminReport service not instanced")
+
+	// to json
+	arIn, err := json.Marshal(adminReport)
+	assert.Nil(err, "AdminReport test data corrupted")
+
+	// call service
+	cs.On("Get", fmt.Sprintf("/v1/admin/reports/%s", adminReport.ID)).Return(arIn, 200, nil)
+	arOut, err := ars.GetAdminReport(adminReport.ID)
+	assert.Nil(err, "Error getting adminReport")
+
+	// exclude time data from comparison
+	adminReport.StartTime = arOut.StartTime
+	adminReport.EndTime = arOut.EndTime
+
+	assert.Equal(*adminReport, *arOut, "GetAdminReport returned different adminReports")
+
+	return arOut
 }
--- api/admin_reports_api_test.go
+++ api/admin_reports_api_test.go
@@ -10,3 +10,10 @@ func TestGetAdminReportList(t *testing.T) {
 	adminReportsIn := testdata.GetAdminReportsData()
 	GetAdminReportListMocked(t, adminReportsIn)
 }
+
+func TestGetAdminReport(t *testing.T) {
+	adminReportsIn := testdata.GetAdminReportsData()
+	for _, adminReportIn := range *adminReportsIn {
+		GetAdminReportMocked(t, &adminReportIn)
+	}
+}
