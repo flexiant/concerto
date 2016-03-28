64b8e4cd7186293428ad3044b40b6263e027af97
Pablo Mercado <odacremolbap@gmail.com>
2016-03-28 17:22:55 +0200

    test uninitialized service for admin resports

--- api/admin_reports_api_test.go
+++ api/admin_reports_api_test.go
@@ -2,9 +2,17 @@ package api
 
 import (
 	"github.com/flexiant/concerto/testdata"
-
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
+
+func TestNewReportServiceNil(t *testing.T) {
+
+	assert := assert.New(t)
+	rs, err := NewReportService(nil)
+	assert.Nil(rs, "Uninitialized service should return nil")
+	assert.NotNil(err, "Uninitialized service should return error")
+}
 
 func TestGetAdminReportList(t *testing.T) {
 	adminReportsIn := testdata.GetAdminReportsData()
